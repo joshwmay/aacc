e0a2f1e2a7a0bea72bffb15e50daa4a9e0794c3f
jmay1 <jmay1@terpmail.umd.edu>
2021-12-30 23:33:21 -0500

    Worked in Jupyter and tweaked some styling
    Worked with data to look for presentable items;
    Attempted to improve page visualization through more specific shading on larger objects

--- js/script.js
+++ js/script.js
@@ -204,82 +204,82 @@ function mapper() {
         fillColor: '#f03',
         fillOpacity: 0.5,
         radius: 50
-    }).addTo(mymap).bindPopup("<b><a href='https://www.aacc.edu/'>Anne Arundel Community College</a></b><br><b>Student Body:</b> 12,655<br>(#3 in Size amongst MD CC's & #8 All of MD)<br><b>$ Per Credit Hour:</b>  $411.80<br><b>Students 25 and Older:</b>  #5<br><b>GI Bill Student%:</b>  5.6%<br>(#2 in Maryland CC's)");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.aacc.edu/'>Anne Arundel Community College</a></b><br><b>Student Body:</b> 12,655<br>(#3 in Size amongst MD CC's & #8 All of MD)<br><b>$ Per Credit Hour:</b>  $411.80<br><b>Students 25 and Older:</b>  #5<br><b>GI Bill Student%:</b>  5.6%<br>(#2 in Maryland CC's)<br><b>Vaccine Req: </b>Required");
     var circle = L.circle([39.3206170291295, -76.6613018002544], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.bccc.edu/'>Baltimore City Community College</a></b><br><b>Student Body:</b> 4,909<br><b>$ Per Credit Hour:</b>  $308.20<br><b>Students 25 and Older:</b>  #1<br><b>GI Bill Student%:</b>  1.8%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.bccc.edu/'>Baltimore City Community College</a></b><br><b>Student Body:</b> 4,909<br><b>$ Per Credit Hour:</b>  $308.20<br><b>Students 25 and Older:</b>  #1<br><b>GI Bill Student%:</b>  1.8%<br><b>Vaccine Req: </b><i>Not listed</i>");
     var circle = L.circle([39.53183213962793, -76.99109095421696], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.allegany.edu/'>Allegany College of Maryland</a></b><br><b>Student Body:</b> 2,584<br><br><b>GI Bill Student%:</b>  1.8%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.allegany.edu/'>Allegany College of Maryland</a></b><br><b>Student Body:</b> 2,584<br><br><b>GI Bill Student%:</b>  1.8%<br><b>Vaccine Req: </b>Required for Housing<br>No for Commuters");
     var circle = L.circle([39.64116925792962, -75.95647399326049], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://cecil.edu/'>Cecil College</a></b><br><b>Student Body:</b> 2,377<br><b>$ Per Credit Hour:</b>  $309.60<br><b>Students 25 and Older:</b>  #9<br><b>GI Bill Student%:</b>  3.6%<br>(#5 in Maryland CC's)");
+    }).addTo(mymap).bindPopup("<b><a href='http://cecil.edu/'>Cecil College</a></b><br><b>Student Body:</b> 2,377<br><b>$ Per Credit Hour:</b>  $309.60<br><b>Students 25 and Older:</b>  #9<br><b>GI Bill Student%:</b>  3.6%<br>(#5 in Maryland CC's)<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([38.95275598286228, -76.08110479258522], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.chesapeake.edu/'>Chesapeake College</a></b><br><b>Student Body:</b> 2,184<br><b>$ Per Credit Hour:</b>  $320.60<br><b>Students 25 and Older:</b>  #13<br><b>GI Bill Student%:</b>  1.6%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.chesapeake.edu/'>Chesapeake College</a></b><br><b>Student Body:</b> 2,184<br><b>$ Per Credit Hour:</b>  $320.60<br><b>Students 25 and Older:</b>  #13<br><b>GI Bill Student%:</b>  1.6%<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([38.55961742722099, -77.00892890675182], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.csmd.edu/'>College of Southern Maryland</a></b><br><b>Student Body:</b> 6,351<br><b>$ Per Credit Hour:</b>  $375.60<br><b>Students 25 and Older:</b>  #11<br><b>GI Bill Student%:</b>  7.5%<br>(#1 in Maryland CC's)");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.csmd.edu/'>College of Southern Maryland</a></b><br><b>Student Body:</b> 6,351<br><b>$ Per Credit Hour:</b>  $375.60<br><b>Students 25 and Older:</b>  #11<br><b>GI Bill Student%:</b>  7.5%<br>(#1 in Maryland CC's)<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([39.35364008477602, -76.4823566374394], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://www.ccbcmd.edu/'>Community College of Baltimore County</a></b><br><b>Student Body:</b> 17,732<br>(#2 in Size amongst MD & CC's #6 All of MD)<br><b>$ Per Credit Hour:</b>  $372.80<br><b>Students 25 and Older:</b>  #2<br><b>GI Bill Student%:</b>  .8%");
+    }).addTo(mymap).bindPopup("<b><a href='http://www.ccbcmd.edu/'>Community College of Baltimore County</a></b><br><b>Student Body:</b> 17,732<br>(#2 in Size amongst MD & CC's #6 All of MD)<br><b>$ Per Credit Hour:</b>  $372.80<br><b>Students 25 and Older:</b>  #2<br><b>GI Bill Student%:</b>  .8%<br><b>Vaccine Req: </b><i>'No, Only required for some health professions students'");
     var circle = L.circle([39.451979960641104, -77.4184624490804], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://www.frederick.edu/'>Frederick Community College</a></b><br><b>Student Body:</b> 6,129<br><b>$ Per Credit Hour:</b>  $389.60<br><b>Students 25 and Older:</b>  #12<br><b>GI Bill Student%:</b>  3.8%<br>(#4 in Maryland CC's)");
+    }).addTo(mymap).bindPopup("<b><a href='http://www.frederick.edu/'>Frederick Community College</a></b><br><b>Student Body:</b> 6,129<br><b>$ Per Credit Hour:</b>  $389.60<br><b>Students 25 and Older:</b>  #12<br><b>GI Bill Student%:</b>  3.8%<br>(#4 in Maryland CC's)<br><b>Vaccine Req: </b>Not required, will be Spring '22");
     var circle = L.circle([39.56088682063832, -79.34275053559213], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.garrettcollege.edu/'>Garret College</a></b><br><b>Student Body:</b> 651<br><b>$ Per Credit Hour:</b>  $332.80<br><b>Students 25 and Older:</b>  #16<br><b>GI Bill Student%:</b>  1.8%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.garrettcollege.edu/'>Garrett College</a></b><br><b>Student Body:</b> 651<br><b>$ Per Credit Hour:</b>  $332.80<br><b>Students 25 and Older:</b>  #16<br><b>GI Bill Student%:</b>  1.8%<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([39.63194874917637, -77.66902870304568], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://www.hagerstowncc.edu/'>Hagerstown Community College</a></b><br><b>Student Body:</b> 3,848<br><b>$ Per Credit Hour:</b>  $264.60<br><b>Students 25 and Older:</b>  #8<br><b>GI Bill Student%:</b>  3.0%");
+    }).addTo(mymap).bindPopup("<b><a href='http://www.hagerstowncc.edu/'>Hagerstown Community College</a></b><br><b>Student Body:</b> 3,848<br><b>$ Per Credit Hour:</b>  $264.60<br><b>Students 25 and Older:</b>  #8<br><b>GI Bill Student%:</b>  3.0%<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([39.56034703621479, -76.28449202024339], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.harford.edu/'>Harford Community College</a></b><br><b>Student Body:</b> 5,705<br><b>$ Per Credit Hour:</b>  $152.60<br><b>Students 25 and Older:</b>  #10<br><b>GI Bill Student%:</b>  2.7%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.harford.edu/'>Harford Community College</a></b><br><b>Student Body:</b> 5,705<br><b>$ Per Credit Hour:</b>  $152.60<br><b>Students 25 and Older:</b>  #10<br><b>GI Bill Student%:</b>  2.7%<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([39.21243733541452, -76.87831298955557], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://www.howardcc.edu/'>Howard Community College</a></b><br><b>Student Body:</b> 9,110<br>(#5 in Size amongst MD CC's & #10 All of MD)<br><b>$ Per Credit Hour:</b>  $301.20<br><b>Students 25 and Older:</b>  #6<br><b>GI Bill Student%:</b>  3.4%");
+    }).addTo(mymap).bindPopup("<b><a href='http://www.howardcc.edu/'>Howard Community College</a></b><br><b>Student Body:</b> 9,110<br>(#5 in Size amongst MD CC's & #10 All of MD)<br><b>$ Per Credit Hour:</b>  $301.20<br><b>Students 25 and Older:</b>  #6<br><b>GI Bill Student%:</b>  3.4%<br><b>Vaccine Req: </b>Not required, will be Spring '22");
     var circle = L.circle([39.10009657780947, -77.157878462514], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.montgomerycollege.edu/'>Montgomery College</a></b><br><b>Student Body:</b> 21,260<br>(#1 in Size amongst MD CC's & #5 All of MD)<br><b>$ Per Credit Hour:</b>  $451.20<br><b>Students 25 and Older:</b>  #7<br><b>GI Bill Student%:</b>  1.7%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.montgomerycollege.edu/'>Montgomery College</a></b><br><b>Student Body:</b> 21,260<br>(#1 in Size amongst MD CC's & #5 All of MD)<br><b>$ Per Credit Hour:</b>  $451.20<br><b>Students 25 and Older:</b>  #7<br><b>GI Bill Student%:</b>  1.7%<br><b>Vaccine Req: </b>Not required, Will be as of 1/7/2022");
     var circle = L.circle([38.88684458754917, -76.82660986257397], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='https://www.pgcc.edu/'>Prince George's Community College</a></b><br><b>Student Body:</b> 11,788<br>(#4 in Size amongst MD CC's #9 All of MD)<br><b>$ Per Credit Hour:</b>  $350.00<br><b>Students 25 and Older:</b>  #3<br><b>GI Bill Student%:</b>  3.1%");
+    }).addTo(mymap).bindPopup("<b><a href='https://www.pgcc.edu/'>Prince George's Community College</a></b><br><b>Student Body:</b> 11,788<br>(#4 in Size amongst MD CC's #9 All of MD)<br><b>$ Per Credit Hour:</b>  $350.00<br><b>Students 25 and Older:</b>  #3<br><b>GI Bill Student%:</b>  3.1%<br><b>Vaccine Req: </b>Required");
     var circle = L.circle([38.374571849605964, -75.49990880489847], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://www.worwic.edu/'>Wor-Wic Community College</a></b><br><b>Student Body:</b> 2,890<br><b>$ Per Credit Hour:</b>  $319.60<br><b>Students 25 and Older:</b>  #4<br><b>GI Bill Student%:</b>  5.1%<br>(#3 in Maryland CC's)");
+    }).addTo(mymap).bindPopup("<b><a href='http://www.worwic.edu/'>Wor-Wic Community College</a></b><br><b>Student Body:</b> 2,890<br><b>$ Per Credit Hour:</b>  $319.60<br><b>Students 25 and Older:</b>  #4<br><b>GI Bill Student%:</b>  5.1%<br>(#3 in Maryland CC's)<br><b>Vaccine Req: </b>Not required");
     var circle = L.circle([39.53177007829051, -76.99130553102033], 6000, {
         color: 'blue',
         fillColor: 'lightblue',
         fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("<b><a href='http://www.carrollcc.edu/'>Carroll Community College</a></b><br><b>Student Body:</b> 3,115<br><b>$ Per Credit Hour:</b>  $313.00<br><b>Students 25 and Older:</b>  #15<br><b>GI Bill Student%:</b>  2.7.%");
+    }).addTo(mymap).bindPopup("<b><a href='http://www.carrollcc.edu/'>Carroll Community College</a></b><br><b>Student Body:</b> 3,115<br><b>$ Per Credit Hour:</b>  $313.00<br><b>Students 25 and Older:</b>  #15<br><b>GI Bill Student%:</b>  2.7.%<br><b>Vaccine Req: </b>Not required");
     return mymap
 }
 
