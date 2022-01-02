33c77069801dc8a2b775efe129e451c090bd41ff
joshwmay <45636119+joshwmay@users.noreply.github.com>
2022-01-01 23:11:09 -0500

    Continuing to format for space
    Working toward making more space with what's available by reconfiguring visuals

--- js/script.js
+++ js/script.js
@@ -226,7 +226,7 @@ function chart3(data1, data2, data3, data4, data5, data6) {
 };
 
 function mapper() {
-    var mymap = L.map('mapid').setView([39.10009657780947, -77.157878462514], 8);
+    var mymap = L.map('mapid').setView([39.10009657780947, -77.157878462514], 7);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         id: 'mapbox/streets-v11',
